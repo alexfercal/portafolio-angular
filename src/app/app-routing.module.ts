d859666678eee06da1a477da1574de69f91f1884
Alex Fer <alexfercal@gmail.com>
2019-02-19 17:33:58 -0500

    Fin seccion 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+// import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// const routes: Routes = [];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
+import { PortafolioComponent} from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+
+const appRoutes: Routes = [
+  {path: 'home', component: PortafolioComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'item', component: ItemComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(appRoutes, { useHash: true} )
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
