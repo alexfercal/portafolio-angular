57c1bc4c80c3ba12e81ca5512c2cd044c91834aa
Alex Fer <alexfercal@gmail.com>
2019-02-20 12:38:01 -0500

    Fin seccion 4

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,11 @@
+
+
+export interface InfoPagina{
+  titulo?: string;
+  email?: string;
+  nombre_corto?: string;
+  pagina_autor?: string;
+  twitter?: string;
+  facebook?: string;
+  equipo_trabajo?: any[];
+}
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: "root"
 })
 export class InfoPaginaService {
-  info: any = {};
+  info: InfoPagina = {};
   cargada = false;
 
   constructor(private http: HttpClient) {
-    console.log("Servicio de infoPaginaListo");
-    this.http.get("/assets/data/data-folder.json").subscribe(resp => {
+    console.log('Servicio de infoPaginaListo');
+    this.http.get('/assets/data/data-folder.json').subscribe((resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
       console.log(resp);
