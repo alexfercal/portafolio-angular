0fe69419821376205f6e895e7c5a1b56409538ab
Alex Fer <alexfercal@gmail.com>
2019-02-21 15:55:50 -0500

    Pre produccion

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,44 +1,72 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Producto } from '../interfaces/item.interface';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Producto } from "../interfaces/item.interface";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ProductosService {
-
   cargando = true;
   productos: Producto[] = [];
   productosFiltrado: Producto[] = [];
 
-  constructor( private http: HttpClient) {
-
+  constructor(private http: HttpClient) {
     this.cargarProductos();
-
   }
 
-  cargarProductos(){
-    this.http.get('https://angular-html-73eb9.firebaseio.com/productos_idx.json')
-    .subscribe((resp: Producto[]) => {
-        // console.log(resp);
-        this.productos = resp;
-        setTimeout(() => {
-          this.cargando = false;
-        }, 3000);
+  cargarProductos() {
+    return new Promise((resolve, reject) => {
+      this.http
+        .get("https://angular-html-73eb9.firebaseio.com/productos_idx.json")
+        .subscribe((resp: Producto[]) => {
+          // console.log(resp);
+          this.productos = resp;
+          setTimeout(() => {
+            this.cargando = false;
+          }, 3000);
+          resolve();
+        });
     });
   }
 
-  getProducto(id: string){
-    return this.http.get(`https://angular-html-73eb9.firebaseio.com/productos/${id}.json`);
+  getProducto(id: string) {
+    return this.http.get(
+      `https://angular-html-73eb9.firebaseio.com/productos/${id}.json`
+    );
   }
 
-  buscarProducto(termino:string){
-    this.productosFiltrado = this.productos.filter( producto => {
-      return true;
-    } );
+  buscarProducto(termino: string) {
+    if (this.productos.length === 0) {
+      this.cargarProductos().then(() => {
+        this.filtrarProductos(termino);
+      });
+    } else {
+      this.filtrarProductos(termino);
+    }
+
+    // this.productosFiltrado = this.productos.filter(producto => {
+    //   return true;
+    // });
 
     console.log(this.productosFiltrado);
   }
 
+  private filtrarProductos(termino: string) {
 
+    this.productosFiltrado = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach(prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if (
+        prod.categoria.indexOf(termino) >= 0 ||
+        tituloLower.indexOf(termino) >= 0
+      ) {
+        this.productosFiltrado.push(prod);
+      }
+    });
+  }
 }
